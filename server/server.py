ce15bb19a97cf77f1f43a8a05c08eabba5180a2f
thanhduycao <thanhduycao1202@gmail.com>
2023-05-30 19:58:18 +0700

    [SERVER-1]feat: Add baseline code for server with /summarize route

--- server/server.py
+++ server/server.py
@@ -1,19 +1,34 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from waitress import serve
 import os
+from text_summarizer.paragraph_summarizer import ParagraphSummarizer
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
+model = ParagraphSummarizer()
+
+
+@app.route("/", methods=["GET", "POST"])
 def hello():
-    return 'Hello, World!'
+    return "Hello, World!"
 
 
-if __name__ == '__main__':
+@app.route("/summarize", methods=["POST"])
+def summarize():
+    print(request.args.get("input"))
+    text_seq = request.args.get("input")
+    out = model.execute(text_seq=text_seq)
+
+    return jsonify({"out": out})
+
+
+if __name__ == "__main__":
     try:
-        port = int(os.environ.get('PORT', 5000))
+        port = int(os.environ.get("PORT", 5000))
     except:
         port = 5000
 
     print("Starting server on port {}".format(port))
-    serve(app, host='0.0.0.0', port=port)
+    serve(app, host="0.0.0.0", port=port)
