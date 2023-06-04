d05b7e4d9da05a02ae32fd07e9398393f4dea258
thanhduycao <thanhduycao1202@gmail.com>
2023-06-04 10:46:44 +0700

    [CLIENT-4]feat: Add keyword extractor UI to the summarizer tool

--- client/src/pages/home-page/components/Summarizer.tsx
+++ client/src/pages/home-page/components/Summarizer.tsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import Card from '@mui/material/Card'
 import Grid from '@mui/material/Grid'
-import { TextField, Divider, Icon } from '@mui/material'
+import { Divider } from '@mui/material'
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import Tooltip from '@mui/material/Tooltip';
 import { CustomSummarizeButton } from '../../../components/CustomButton/CustomSummarizeButton';
 import { ParagraphProgress } from '../../../components/ParagraphProgress/ParagraphProgress';
-import { makeStyles } from '@mui/styles'
-import absSumAPI from '../../../api/absSumAPI';
 import { CustomTextField } from '../../../components/CustomTextField/CustomTextField';
-import { json } from 'stream/consumers';
+import { KeywordPaper } from '../../../components/KeywordPaper/KeywordPaper';
+import { makeStyles } from '@mui/styles'
+import absSumAPI from '../../../api/absSumAPI.ts';
+import keyExtractAPI from '../../../api/keyExtractAPI.ts';
+import { error } from 'console';
 
 const useStyles = makeStyles({
   root: {
@@ -24,8 +26,9 @@ const useStyles = makeStyles({
   },
   inputGrid: {
     width: '100%',
-    height: '100%',
-    padding: '1rem'
+    maxHeight: '100%',
+    padding: '1rem',
+    justifyContent: 'space-between',
   },
   inputContainer: {
     height: '100%'
@@ -70,10 +73,14 @@ const useStyles = makeStyles({
 
 const inputLabel = "Enter or paste your text and press \"Summarize\""
 const introText = "The Summarizer condenses articles, papers, and other documents into a bulleted Key Sentences list or into a new paragraph"
+const maxInputText = 512
+const topNKeywords = 5
+
 export const Summarizer: React.FC = () => {
   const classes = useStyles()
   const [inputText, setInputText] = React.useState<string>("")
   const [outputText, setOutputText] = React.useState<string>("")
+  const [keywords, setKeywords] = React.useState<string[]>([])
   const [isProcessing, setIsProcessing] = React.useState<boolean>(false)
 
   async function postSummaryAPI() {
@@ -81,6 +88,19 @@ export const Summarizer: React.FC = () => {
     console.log(summary.out)
     setOutputText(summary.out)
     setIsProcessing(false)
+  }
+
+  async function postKeywordsExtractionAPI() {
+    const keywords = await keyExtractAPI(inputText, topNKeywords)
+    console.log(keywords.keywords)
+    setKeywords(keywords.keywords)
+  }
+
+  function isAbleToSummarize(): boolean {
+    if (inputText.split(" ").length > maxInputText) {
+      return false
+    }
+    return true
   }
 
   return (
@@ -103,8 +123,15 @@ export const Summarizer: React.FC = () => {
               multiline
               placeholder={inputLabel}
               value={inputText}
-              onChange={(e) => setInputText(e.target.value)}
+              onChange={(e) => {
+                if (e.target.value === "") {
+                  setKeywords([])
+                }
+                setInputText(e.target.value)
+              }
+              }
             />
+            <KeywordPaper keywords={keywords} />
           </Grid>
           <Divider orientation="vertical" flexItem sx={{ mr: '-1px' }} />
           <Grid item xs={6} className={classes.inputGrid}>
@@ -122,20 +149,23 @@ export const Summarizer: React.FC = () => {
           <Grid item xs={6}>
             <Grid container className={classes.leftFooterContainer}>
               <Grid item xs={6}>
-                <div>
-                  Upload articles
+                <div
+                  style={{ color: isAbleToSummarize() ? "black" : "red" }}
+                >
+                  {inputText.split(" ").length - 1} words | {maxInputText} words max
                 </div>
               </Grid>
               <Grid item xs={6}>
                 <div className={classes.summarizeButton}>
                   <CustomSummarizeButton
                     color='primary'
-                    disabled={isProcessing}
+                    disabled={isProcessing || !isAbleToSummarize()}
                     onClick={() => {
                       try {
                         setOutputText("")
                         setIsProcessing(true)
                         postSummaryAPI()
+                        postKeywordsExtractionAPI()
                       } catch (err) {
                         console.log(err)
                       }
@@ -148,8 +178,7 @@ export const Summarizer: React.FC = () => {
           </Grid>
           <Divider orientation="vertical" flexItem sx={{ mr: '-1px' }} />
           <Grid item xs={6} className={classes.rightFooterContainer}>
-            {/* Second section */}
-            Second section
+            {outputText.split(" ").length - 1} words | {outputText.split(".").length - 1} sentences
           </Grid>
         </Grid>
       </Grid>
