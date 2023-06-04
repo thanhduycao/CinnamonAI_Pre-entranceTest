60fcb431128ae7324bfa09113346b0907a36a51b
thanhduycao <thanhduycao1202@gmail.com>
2023-06-04 10:50:18 +0700

    [SERVER-4]: Add route extract keyword for server

--- server/server.py
+++ server/server.py
@@ -3,25 +3,42 @@ from flask_cors import CORS
 from waitress import serve
 import os
 from text_summarizer.paragraph_summarizer import ParagraphSummarizer
+from text_summarizer.keyword_extractor import KeywordExtractor
 
 app = Flask(__name__)
 CORS(app)
 
 model = ParagraphSummarizer()
+keywordExtractor = KeywordExtractor()
 
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
-    return "Hello, World!"
+    return "Health check: OK"
 
 
 @app.route("/summarize", methods=["POST"])
 def summarize():
-    print(request.args.get("input"))
-    text_seq = request.args.get("input")
+    data = request.get_json()
+    text_seq = data["input"]
     out = model.execute(text_seq=text_seq)
 
     return jsonify({"out": out})
+
+
+@app.route("/keywords", methods=["POST"])
+def keywords():
+    data = request.get_json()
+    text_seq = data["input"]
+    top_n = data["top_n"]
+    out = keywordExtractor.execute(docs=text_seq, top_n=5, keyphrase_ngram_range=[1, 2])
+    keywords = []
+    if len(out) < top_n:
+        top_n = len(out)
+    for i in range(top_n):
+        keywords.append(out[i][0])
+
+    return jsonify({"keywords": keywords})
 
 
 if __name__ == "__main__":
